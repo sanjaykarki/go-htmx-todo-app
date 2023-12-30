1939a86bdb610cdcb56eaf0251325d88ab7b0491
Sanjay Karki <sanjaykarki@live.com>
2023-12-30 17:40:33 +0100

    Imported Chi, Vite and HTMX

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	fmt.Println("Running server on port 3000....")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+		tmpl, _ := template.New("").ParseFiles("templates/index.html")
+		tmpl.ExecuteTemplate(w, "Base", nil)
+	})
+	http.ListenAndServe("localhost:3000", r)
+}
